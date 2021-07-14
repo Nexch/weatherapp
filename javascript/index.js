c94664c83b2775b1b2738df15ad3db7c8c60f7ef
Nexch <mauricio.t.1999@gmail.com>
2021-07-14 08:53:40 -0400

    fix linters errors

--- javascript/index.js
+++ javascript/index.js
@@ -1,35 +1,32 @@
-var lol
-
-function weather(city) {
-	var settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=f43223591fa77f00dda54c1439ff73ff`,
-		"method": "GET",
-	}
-
-
-	$.ajax(settings).done(function (response) {
-		lol = response;
-		display(lol.main.temp, "date", "C*")
-		display(lol.name, "country", "Country")
-		display(lol.weather[0].main, "weather", "Climate")
-	});
-
-}
+let lol;
 
 function display(lol, id, m) {
-	document.getElementById(`${id}`).innerHTML = `${m}:${lol}`;
+  document.getElementById(`${id}`).innerHTML = `${m}:${lol}`;
+}
+
+function weather(city) {
+  const settings = {
+    async: true,
+    crossDomain: true,
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=f43223591fa77f00dda54c1439ff73ff`,
+    method: 'GET',
+  };
+
+  $.ajax(settings).done((response) => {
+    lol = response;
+    display(lol.main.temp, 'date', 'C*');
+    display(lol.name, 'country', 'Country');
+    display(lol.weather[0].main, 'weather', 'Climate');
+  });
 }
 
 function formTemp() {
-	let form = document.getElementById('form');
-	name = document.getElementById('city').value
-	document.getElementById('city').value = ""
-	weather(name);
+  const name = document.getElementById('city').value;
+  document.getElementById('city').value = '';
+  weather(name);
 }
 
-document.querySelector('.form')?.addEventListener('submit', e => {
+document.querySelector('.form')?.addEventListener('submit', (e) => {
   e.preventDefault();
   formTemp();
 });
