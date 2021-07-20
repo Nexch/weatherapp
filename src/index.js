0f7a85b16f46bafc5f01e0dce5eb2e6ecf32f3c9
Nexch <mauricio.t.1999@gmail.com>
2021-07-20 11:08:35 -0400

    fix errors for the changes required

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 let lol;
 
-function display(lol, id, m) {
+const display = (lol, id, m) => {
   document.getElementById(`${id}`).innerHTML = `${m}:${lol}`;
-}
+  const temp = document.getElementById('temps').value;
+  if (temp === 'F' && id === 'date') {
+    lol = Math.round((lol * (9 / 5)) + 32);
+    document.getElementById(`${id}`).innerHTML = `F: ${lol}`;
+  }
 
-function weather(city) {
+  if (id === 'weather') {
+    $('body').css('background-image', `url(img/${lol}.jpg)`);
+  }
+};
+
+const weather = (city) => {
   const settings = {
     async: true,
     crossDomain: true,
     url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=f43223591fa77f00dda54c1439ff73ff`,
     method: 'GET',
+    error(xhr, status, error) {
+      const errorMessage = `${xhr.status}: ${xhr.statusText}`;
+      display(`Error - ${errorMessage}`, 'error', '');
+    },
   };
 
   $.ajax(settings).done((response) => {
@@ -18,13 +31,13 @@ function weather(city) {
     display(lol.name, 'country', 'Country');
     display(lol.weather[0].main, 'weather', 'Climate');
   });
-}
+};
 
-function formTemp() {
+const formTemp = () => {
   const name = document.getElementById('city').value;
   document.getElementById('city').value = '';
   weather(name);
-}
+};
 
 document.querySelector('.form')?.addEventListener('submit', (e) => {
   e.preventDefault();
